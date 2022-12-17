cbb723c73fa35cbb5fd3b094c5fd855fc823cb4e
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2022-12-17 18:15:51 -0300

    aula 03 finalizada

--- src/application/use-cases/get-recipient-notifications.spec.ts
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -1,11 +1,11 @@
 import { makeNotification } from '@test/factories/notification-factory';
 import { inMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
-import { CountRecipientNotifications } from './count-recipient-notifications';
+import { GetRecipientNotifications } from './get-recipient-notifications';
 
 describe('Count recipient notifications', () => {
-  it('should be able to count recipient notifications', async () => {
+  it('should be able to get recipient notifications', async () => {
     const notificationsRepository = new inMemoryNotificationsRepository();
-    const countRecipientNotifications = new CountRecipientNotifications(
+    const getRecipientNotifications = new GetRecipientNotifications(
       notificationsRepository,
     );
 
@@ -21,10 +21,16 @@ describe('Count recipient notifications', () => {
       makeNotification({ recipientId: 'this-exactly-recipient' }),
     );
 
-    const { count } = await countRecipientNotifications.execute({
+    const { notifications } = await getRecipientNotifications.execute({
       recipientId: 'this-exactly-recipient',
     });
 
-    expect(count).toEqual(2);
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'this-exactly-recipient' }),
+        expect.objectContaining({ recipientId: 'this-exactly-recipient' }),
+      ]),
+    );
   });
 });
